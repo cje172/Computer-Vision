16bdc2746cd2e8f8f21e076393e6d0ac9d6832e2
Jieun Choi <cje172@naver.com>
2022-04-05 21:43:06 +0900

    입력 영상의 역상 구하기_Gray-scale

---
+++ Image Processing/project/Inverse_Gray-scale/Source.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+	int i, j;
+	unsigned char OrgImg[256][256];
+
+	// 디스크에서 영상데이터 읽기
+	errno_t err;
+	FILE *infile = NULL;
+	FILE *outfile = NULL;
+
+	err = fopen_s(&infile, "coin.raw", "rb");
+
+	if (err != 0) // 성공적으로 열면 err은 0 반환
+	{
+		printf("File open error!!");
+		return -1;
+	}
+	fread(OrgImg, sizeof(char), 256 * 256, infile);
+	fclose(infile);
+
+	// 역상계산을 위한 영상처리
+	for (i = 0; i < 256; i++)
+	{
+		for (j = 0; j < 256; j++)
+		{
+			OrgImg[i][j] = 255 - OrgImg[i][j];
+		}
+	}
+
+	// 하드디스크에 영상데이터 쓰기
+	err = fopen_s(&outfile, "coin_inv.raw", "wb");
+	fwrite(OrgImg, sizeof(char), 256 * 256, outfile);
+	fclose(outfile);
+
+	return 0;
+}
